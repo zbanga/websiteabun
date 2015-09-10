58df8a2c7bbc53ac219de2495ab81c96907af162
zbanga <ben10_zhang@hotmail.com>
2015-09-11 00:17:55 +1000

    Edited Config file
    From localhost -> http://abundata.azurewebsites.net/

--- includes/config.php
+++ includes/config.php
@@ -3,11 +3,11 @@
 
 //mysql connection information
 
-$hostname_contacts = "localhost";  
+$hostname_contacts = "http://abundata.azurewebsites.net/";  
 $database_contacts = "database_name"; //The name of the database
 $username_contacts = "database_username"; //The username for the database
 $password_contacts = "database_password"; // The password for the database
-$contacts = mysql_connect($hostname_contacts, $username_contacts, $password_contacts) or trigger_error(mysql_error(),E_USER_ERROR); 
+$contacts = mysql_connect($hostname_contacts, $username_contacts, $password_contacts) or trigger_error(mysql_error(),E_USER_ERROR);
 mysql_select_db($database_contacts, $contacts);
 
 //
